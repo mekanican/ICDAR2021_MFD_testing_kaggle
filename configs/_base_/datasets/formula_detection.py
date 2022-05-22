cb20375d4c57a826c96bb43d512769d9e46e57ca
Vo Trong Nghia <31099373+mekanican@users.noreply.github.com>
2022-05-22 22:26:17 +0700

    MOD CROP

--- configs/_base_/datasets/formula_detection.py
+++ configs/_base_/datasets/formula_detection.py
@@ -7,7 +7,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1447, 2048), keep_ratio=True),
-    dict(type='RandomCrop', crop_size=(1600, 1440)),
+    dict(type='RandomCrop', crop_size=(1200, 1120)),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
